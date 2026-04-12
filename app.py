51ab1d2d4fa29a5cc032c92de7e773d2f7fbc8be
Jacob  Janoff <92239316+harmfulgaming@users.noreply.github.com>
2026-04-11 23:41:38 -0400

    Change app port from 8080 to 5000

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def buy_item(item_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 @app.route('/scrap/<int:item_id>', methods=['POST'])
 def scrap_item(item_id):
     ledger = get_ledger()
